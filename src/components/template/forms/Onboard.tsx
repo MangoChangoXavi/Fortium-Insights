0610c7d0f964862c83a482521777c4b4d8b56102
Xavi Monterroso <jvmonteros98@gmail.com>
2024-02-04 19:12:27 -0600

    fixed bug

--- src/components/template/forms/Onboard.tsx
+++ src/components/template/forms/Onboard.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import { ReCAPTCHA } from "react-google-recaptcha";
+import ReCAPTCHA from "react-google-recaptcha";
 
 import {
   Form,
