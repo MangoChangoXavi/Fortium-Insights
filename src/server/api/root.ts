e3995c1dbf535172ebc51732b652133d9346acf1
Xavi Monterroso <jvmonteros98@gmail.com>
2024-02-04 18:06:08 -0600

    removed unused files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,21 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { reservationRouter } from "./routers/reservation";
-import { eventRouter } from "./routers/event";
-import { lotRouter } from "./routers/lot";
-import { userRouter } from "./routers/user";
-import { dashboardRouter } from "./routers/dashboard";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  reservation: reservationRouter,
-  event: eventRouter,
-  lot: lotRouter,
-  user: userRouter,
-  dashboard: dashboardRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
