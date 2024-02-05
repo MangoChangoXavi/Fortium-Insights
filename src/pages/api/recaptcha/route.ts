dbf0b0f658989aba75ccd33c288b29925c3c61a3
Xavi Monterroso <jvmonteros98@gmail.com>
2024-02-04 19:25:20 -0600

    adding post

--- src/pages/api/recaptcha/route.ts
+++ src/pages/api/recaptcha/route.ts
@@ -1,4 +1,6 @@
-export async function verifyCaptcha(token: string | null) {
+export default async function POST(req: Request) {
+  const body = (req.body as unknown as Record<string, string>) ?? "";
+  const token = body.token;
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
 
