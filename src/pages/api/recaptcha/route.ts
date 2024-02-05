a6fd51adbe65916927dd29f2e79d05d72430b402
Xavi Monterroso <jvmonteros98@gmail.com>
2024-02-05 15:46:53 -0600

    fixed bug

--- src/pages/api/recaptcha/route.ts
+++ src/pages/api/recaptcha/route.ts
@@ -1,21 +1,15 @@
 import { type NextApiRequest } from "next";
-import { use } from "react";
 
 export default async function POST(req: NextApiRequest) {
   const data = await req.body;
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
   const token = data.token;
-  const user_ip = req.headers["x-forwarded-for"] ?? req.socket.remoteAddress;
   if (!token) throw new Error("No token provided");
-  if (!user_ip) throw new Error("No user ip provided");
 
-  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}&remoteip=${
-    user_ip as string
-  }`;
-  console.log("url", url);
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret='${secretKey}'&response='${token}'`;
 
   const response = await fetch(url, {
-    method: "POST",
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
@@ -26,7 +20,7 @@ export default async function POST(req: NextApiRequest) {
   if (responseData.success) {
     return "success!";
   } else {
-    console.error(responseData);
+    console.log(responseData);
     throw new Error("Failed Captcha");
   }
 }
