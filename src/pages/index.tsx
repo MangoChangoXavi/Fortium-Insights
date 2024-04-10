1db78e0d2a2776c59d9c89a130afa4212eac5122
Javier Monterroso <jvmonteros98@gmail.com>
2024-04-09 23:15:02 -0300

    REMOVED AD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -111,11 +111,6 @@ export default function Index() {
 
   return (
     <>
-      <ResponsiveAdUnit
-        publisherId="pub-6713030727031078"
-        slotId="5133228323"
-        type="after-home-hero"
-      />
       <Header />
       <div className="relative h-48 w-full md:h-96">
         <Image
