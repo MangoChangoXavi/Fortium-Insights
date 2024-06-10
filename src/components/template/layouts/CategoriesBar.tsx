53c58e551ed85a3fb584ed9c3850d0f71db7d2bf
Javier Monterroso <jvmonteros98@gmail.com>
2024-06-10 17:51:06 -0600

    chore: Refactor CategoriesBar component and add loading state
    Refactor the CategoriesBar component to improve code readability and maintainability. Add a loading state to display a loader while the category is being selected.

--- src/components/template/layouts/CategoriesBar.tsx
+++ src/components/template/layouts/CategoriesBar.tsx
@@ -2,28 +2,48 @@ import Image from "next/image";
 import React from "react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { categories } from "~/lib/categories";
+import { useSearchStore } from "~/stores/useSearchStore";
+import { useRouter } from "next/router";
+import { Loader } from "~/components/system/layouts/Loader";
 
 export const CategoriesBar = () => {
+  const { setCategoryId } = useSearchStore();
+  const [loading, setLoading] = React.useState(false);
+  const router = useRouter();
+
+  const handleCategoryClick = async (categoryId: number) => {
+    setLoading(true);
+    setCategoryId(categoryId);
+    await router.push("/search");
+  };
+
   return (
     <ScrollArea className="w-screen whitespace-nowrap ">
-      <div className="flex w-max space-x-4 p-4">
-        {categories.map((category, index) => (
-          <div
-            key={index}
-            className="group inline-flex cursor-pointer  flex-col items-center justify-start gap-4"
-          >
-            <Image
-              alt="category"
-              className="h-[100px] w-[100px] rounded-full transition duration-150 ease-in-out group-hover:scale-110"
-              src={category.imageUrl}
-              width={100}
-              height={100}
-            />
-            <div className="font-['Noto Sans JP'] text-base font-medium text-blue-950 transition duration-150 ease-in-out group-hover:scale-110">
-              {category.name}
-            </div>
+      <div className="flex w-max  space-x-4 p-4">
+        {loading ? (
+          <div className="flex w-screen items-center justify-center">
+            <Loader />
           </div>
-        ))}
+        ) : (
+          categories.map((category, index) => (
+            <button
+              onClick={() => handleCategoryClick(index)}
+              key={index}
+              className="group inline-flex cursor-pointer  flex-col items-center justify-start gap-4"
+            >
+              <Image
+                alt="category"
+                className="h-[100px] w-[100px] rounded-full transition duration-150 ease-in-out group-hover:scale-110"
+                src={category.imageUrl}
+                width={100}
+                height={100}
+              />
+              <div className="font-['Noto Sans JP'] text-base font-medium text-blue-950 transition duration-150 ease-in-out group-hover:scale-110">
+                {category.name}
+              </div>
+            </button>
+          ))
+        )}
       </div>
       <ScrollBar orientation="horizontal" />
     </ScrollArea>
